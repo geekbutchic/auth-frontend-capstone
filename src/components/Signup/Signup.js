99f3512240ec34e60e6955128b7191e4e00a55b0
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-27 10:47:59 -0400

    setup files, setup terminal commands, erase not needed folders, reset css, begin signup skeleton

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export class Signup extends Component {
+    render() {
+        return (
+            <div className="container">
+                
+            </div>
+        )
+    }
+}
+
+export default Signup
